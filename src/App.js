059b4a70544e0ca1568db2afdf33a5af1ee4b9e4
Chizzy Meka <chizzymeka@yahoo.co.uk>
2020-01-27 16:30:09 +0000

    Reacty Components Demo.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+class Car extends React.Component {
+	render() {
+		return <h2>Hi, I am a Car!</h2>;
+	}
 }
 
-export default App;
+export default Car;
